fd93aa6642fa9e008b47bb65e40bb905974fc30d
Andrew Zuo <andrew@andrewzuo.com>
2023-04-01 18:09:51 -0600

    Added a German version of the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,51 +1,82 @@
 import 'package:auto_hyphenating_text/auto_hyphenating_text.dart';
 import 'package:flutter/material.dart';
+import 'package:hyphenator_impure/hyphenator.dart';
 
 void main() {
-  runApp(const MyApp());
+	runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+	const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Auto Hyphenating Text Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Auto Hyphenating Text Demo'),
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		return MaterialApp(
+			title: 'Auto Hyphenating Text Demo',
+			theme: ThemeData(
+				primarySwatch: Colors.blue,
+			),
+			home: const GermanExample(title: 'Auto Hyphenating Text Demo'),
+		);
+	}
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class GermanExample extends StatefulWidget {
+	const GermanExample({super.key, required this.title});
 
-  final String title;
+	final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+	@override
+	State<GermanExample> createState() => _GermanExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _GermanExampleState extends State<GermanExample> {
 
-  @override
-  void initState() {
-    super.initState();
-    initHyphenation();
-  }
+	@override
+	void initState() {
+		super.initState();
+		initHyphenation(DefaultResourceLoaderLanguage.de1996);
+	}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: AutoHyphenatingText('Resize this window to see autohyphenating text in action.', style: Theme.of(context).textTheme.titleLarge, maxLines: 2),
-      ),
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: Text(widget.title),
+			),
+			body: Center(
+				child: AutoHyphenatingText('Ändern Sie die Größe dieses Fensters, um die automatische Silbentrennung in Aktion zu sehen.', style: Theme.of(context).textTheme.titleLarge),
+			),
+		);
+	}
+}
+
+class EnglishExample extends StatefulWidget {
+	const EnglishExample({super.key, required this.title});
+
+	final String title;
+
+	@override
+	State<EnglishExample> createState() => _EnglishExampleState();
+}
+
+class _EnglishExampleState extends State<EnglishExample> {
+
+	@override
+	void initState() {
+		super.initState();
+		initHyphenation();
+	}
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: Text(widget.title),
+			),
+			body: Center(
+				child: AutoHyphenatingText('Resize this window to see autohyphenating text in action.', style: Theme.of(context).textTheme.titleLarge),
+			),
+		);
+	}
 }
